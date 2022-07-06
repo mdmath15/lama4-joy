dae642f4c7e1c34725c1c8726bf2f14e9cc7dddb
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-06 20:40:09 -0300

    deploy

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -17,7 +17,7 @@ export abstract class BaseDatabase {
                   port: 3306 || process.env.PORT,
                   user: process.env.DB_USER,
                   password: process.env.DB_PASSWORD,
-                  database: process.env.DB_NAME,
+                  database: process.env.DB_SCHEMA,
                 },
               });        
         }
