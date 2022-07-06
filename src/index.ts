69365bd228daa0109d02bb64bbd4189187a2db23
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-06 20:36:21 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,11 @@ app.use("/band",bandRouter);
 app.use("/show",showRouter);
 app.use("/ticket",ticketRouter)
 app.use("/gallery",galleryRouter)
-const server = app.listen(3003, () => {
-    if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Servidor rodando em http://localhost:${address.port}`);
-    } else {
-      console.error(`Falha ao rodar o servidor.`);
-    }
-  });
+const server = app.listen(process.env.PORT ||3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo
+    console.log(`Servidor rodando em http://localhost:${address.port}`)
+  } else {
+    console.error(`Falha ao rodar o servidor.`)
+  }
+})
